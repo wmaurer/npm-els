c0d4e0c900c091b520c1f6f71fe61a9fe69d5461
Wayne Maurer <wayne@waynemaurer.net>
2023-12-08 20:18:24 +0100

    add language-service and example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log('Hello World');
+import * as Effect from 'effect/Effect';
+import { pipe } from 'effect/Function';
+
+// Rewrite to Effect.gen
+
+export async function refactorMe(arg: string) {
+  return await Promise.resolve(1);
+}
+
+// Toggle lazy const
+
+const test1 = 1;
+
+const test2 = () => 1;
+
+const test3 = () => {
+  console.log('Hello');
+};
+
+// Rewrite to datafirst
+
+const test = pipe(
+  Effect.succeed('Hello'),
+  Effect.flatMap((_) => Effect.log(_)),
+  Effect.zipRight(Effect.succeed(42)),
+  Effect.map((_) => _ * 2)
+);
